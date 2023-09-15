f473f61ddaa751cb53f7226892f21a0eb326898a
MarinaKS <116084880+MarinaKS@users.noreply.github.com>
2023-09-15 22:28:12 +0300

    Initial commit

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,85 +1,12 @@
 package ru.practicum.shareit.booking;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-import ru.practicum.shareit.booking.dto.BookingDto;
-import ru.practicum.shareit.booking.dto.BookingMapper;
-import ru.practicum.shareit.booking.dto.BookingResponseDto;
-import ru.practicum.shareit.booking.model.Booking;
-import ru.practicum.shareit.booking.service.BookingService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import javax.validation.constraints.PositiveOrZero;
-import java.util.ArrayList;
-import java.util.List;
-
-import static ru.practicum.shareit.Consts.X_SHARER_USER_ID;
-
-@Validated
+/**
+ * TODO Sprint add-bookings.
+ */
 @RestController
-@RequiredArgsConstructor
-@Slf4j
 @RequestMapping(path = "/bookings")
 public class BookingController {
-    private final BookingService bookingService;
-
-    @PostMapping
-    public BookingResponseDto addBooking(@RequestHeader(X_SHARER_USER_ID) long userId, @RequestBody @Valid BookingDto bookingDto) {
-        Booking booking = BookingMapper.toBooking(bookingDto, userId);
-        booking.setStatus(Status.WAITING);
-        log.info(booking.getStart() + "     " + booking.getEnd());
-        return BookingMapper.toBookingResponseDto(bookingService.addBooking(booking));
-    }
-
-    @PatchMapping("/{bookingId}")
-    public BookingResponseDto approveStatus(@RequestHeader(X_SHARER_USER_ID) long userId, @PathVariable long bookingId,
-                                            @RequestParam("approved") boolean approved) {
-        Booking booking = bookingService.approveStatus(userId, bookingId, approved);
-        return BookingMapper.toBookingResponseDto(booking);
-    }
-
-    @GetMapping("/{bookingId}")
-    public BookingResponseDto getBooking(@RequestHeader(X_SHARER_USER_ID) long userId, @PathVariable long bookingId) {
-        Booking booking = bookingService.getBooking(userId, bookingId);
-        return BookingMapper.toBookingResponseDto(booking);
-    }
-
-    @GetMapping
-    public List<BookingResponseDto> getBookingsByUserIdSorted(
-            @RequestHeader(X_SHARER_USER_ID) long userId,
-            @RequestParam(value = "state", defaultValue = "ALL") @Valid State state,
-            @RequestParam(value = "from", required = false) @PositiveOrZero Integer from,
-            @RequestParam(value = "size", required = false) @PositiveOrZero Integer size) {
-        if (from == null && size == null) {
-            from = 0;
-            size = Integer.MAX_VALUE;
-        }
-        List<Booking> bookings = bookingService.getBookingsByUserIdSorted(userId, state, from, size);
-        List<BookingResponseDto> bookingDtos = new ArrayList<>();
-        for (Booking booking : bookings) {
-            bookingDtos.add(BookingMapper.toBookingResponseDto(booking));
-        }
-        return bookingDtos;
-    }
-
-    @GetMapping("/owner")
-    public List<BookingResponseDto> getBookingsByItems(
-            @RequestHeader(X_SHARER_USER_ID) long userId,
-            @RequestParam(value = "state", defaultValue = "ALL") State state,
-            @RequestParam(value = "from", required = false) @PositiveOrZero Integer from,
-            @RequestParam(value = "size", required = false) @PositiveOrZero Integer size) {
-        if (from == null && size == null) {
-            from = 0;
-            size = Integer.MAX_VALUE;
-            ;
-        }
-        List<Booking> bookings = bookingService.getBookingsByItems(userId, state, from, size);
-        List<BookingResponseDto> bookingDtos = new ArrayList<>();
-        for (Booking booking : bookings) {
-            bookingDtos.add(BookingMapper.toBookingResponseDto(booking));
-        }
-        return bookingDtos;
-    }
 }
--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,28 +1,7 @@
 package ru.practicum.shareit.item.model;
 
-import lombok.*;
-import ru.practicum.shareit.request.model.ItemRequest;
-
-import javax.persistence.*;
-
-@Data
-@Entity
-@Table(name = "items", schema = "public")
-@Setter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
+/**
+ * TODO Sprint add-controllers.
+ */
 public class Item {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String description;
-    @Column(name = "is_available")
-    private Boolean isAvailable;
-    @Column(name = "owner_id")
-    private Long ownerId;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "request_id")
-    private ItemRequest request;
 }
