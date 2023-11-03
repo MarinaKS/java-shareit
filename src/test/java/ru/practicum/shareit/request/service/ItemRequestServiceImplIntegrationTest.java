d11c208aa8a0be1cc68eca3a7e5de16abcdc6e93
Satori <kotko.ms@yandex.ru>
2023-11-04 02:45:45 +0300

    add item-requests and tests

--- src/test/java/ru/practicum/shareit/request/service/ItemRequestServiceImplIntegrationTest.java
+++ src/test/java/ru/practicum/shareit/request/service/ItemRequestServiceImplIntegrationTest.java
@@ -32,43 +32,64 @@ public class ItemRequestServiceImplIntegrationTest {
 
     @Test
     public void testAddRequest() {
+        // Создаем пользователя
         User user = new User();
         user.setName("User");
         user.setEmail("user@example.com");
         userRepository.save(user);
+
+        // Создаем запрос на предмет с описанием
         ItemRequest itemRequest = new ItemRequest();
         itemRequest.setRequestor(user);
         itemRequest.setDescription("Request Description");
+
+        // Вызываем метод addRequest
         ItemRequest addedRequest = itemRequestService.addRequest(itemRequest);
+
+        // Получаем ID добавленного запроса
         Long requestId = addedRequest.getId();
 
+        // Проверяем, что запрос был успешно добавлен
         assertNotNull(requestId);
+
+        // Проверяем, что запрос с таким ID существует в базе данных
         assertTrue(itemRequestRepository.existsById(requestId));
     }
 
     @Test
     public void testGetItemRequestsByUserSorted() {
+        // Создаем пользователя
         User user = new User();
         user.setName("User");
         user.setEmail("user@example.com");
         userRepository.save(user);
+
+        // Создаем запросы на предмет для пользователя с описанием
         ItemRequest request1 = new ItemRequest();
         request1.setRequestor(user);
         request1.setDescription("Request 1 Description");
         itemRequestRepository.save(request1);
+
         ItemRequest request2 = new ItemRequest();
         request2.setRequestor(user);
         request2.setDescription("Request 2 Description");
         itemRequestRepository.save(request2);
+
+        // Получаем ID пользователя
         Long userId = user.getId();
+
+        // Вызываем метод getItemRequestsByUserSorted
         List<ItemRequestDto> itemRequests = itemRequestService.getItemRequestsByUserSorted(userId);
 
+        // Проверяем, что список запросов не пустой
         assertFalse(itemRequests.isEmpty());
+        // Проверяем, что список запросов содержит ожидаемое количество запросов
         assertEquals(2, itemRequests.size());
     }
 
     @Test
     public void testGetItemRequestsSorted() {
+        // Создаем пользователя
         User user = new User();
         user.setName("User");
         user.setEmail("user@example.com");
@@ -90,7 +111,8 @@ public class ItemRequestServiceImplIntegrationTest {
         Long userId = user.getId();
         Long anotherUserId = anotherUser.getId();
 
-        List<ItemRequestDto> itemRequests = itemRequestService.getItemRequestsSorted(anotherUserId, 0, 10);
+        List<ItemRequestDto> itemRequests = itemRequestService.
+                getItemRequestsSorted(anotherUserId, 0, 10);
 
         assertFalse(itemRequests.isEmpty());
         assertEquals(2, itemRequests.size());
@@ -98,19 +120,26 @@ public class ItemRequestServiceImplIntegrationTest {
 
     @Test
     public void testGetItemRequestById() {
+        // Создаем пользователя
         User user = new User();
         user.setName("User");
         user.setEmail("user@example.com");
         userRepository.save(user);
+
+        // Создаем запрос на предмет для пользователя с описанием
         ItemRequest request = new ItemRequest();
         request.setRequestor(user);
         request.setDescription("Request Description");
         itemRequestRepository.save(request);
+
+        // Получаем ID пользователя и запроса
         Long userId = user.getId();
         Long requestId = request.getId();
 
+        // Вызываем метод getItemRequestById
         ItemRequestDto itemRequestDto = itemRequestService.getItemRequestById(userId, requestId);
 
+        // Проверяем, что полученный запрос совпадает с ожидаемым запросом
         assertNotNull(itemRequestDto);
     }
 }
