ebe9986a3d26cc5b42ef62a4bfc20639d8630fad
Satori <kotko.ms@yandex.ru>
2023-11-04 03:48:58 +0300

    add tests for coverage and style

--- src/main/java/ru/practicum/shareit/exceptions/UpdateItemException.java
+++ src/main/java/ru/practicum/shareit/exceptions/UpdateItemException.java
@@ -1,22 +1,8 @@
 package ru.practicum.shareit.exceptions;
 
 public class UpdateItemException extends RuntimeException {
-    public UpdateItemException() {
-    }
 
     public UpdateItemException(String message) {
         super(message);
-    }
-
-    public UpdateItemException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public UpdateItemException(Throwable cause) {
-        super(cause);
-    }
-
-    public UpdateItemException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
