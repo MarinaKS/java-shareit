43b38ecedf20f242816d4fa45167df5cf4b6c6da
Satori <kotko.ms@yandex.ru>
2023-12-14 03:17:12 +0300

    fix

--- server/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,7 +1,21 @@
 package ru.practicum.shareit.booking.dto;
 
-/**
- * TODO Sprint add-bookings.
- */
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+import ru.practicum.shareit.booking.Status;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class BookingDto {
+    private Long id;
+    @NonNull
+    private LocalDateTime start;
+    @NonNull
+    private LocalDateTime end;
+    @NonNull
+    private Long itemId;
+    private Status status;
 }
